751359044fc1e4d5fd246f88f76fd824e1d628f2
Yago Marques <yagovictormarques@gmail.com>
2023-06-12 08:45:58 -0300

    first commit

---
+++ BuddyCare Watch App/BuddyCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuddyCareApp.swift
+//  BuddyCare Watch App
+//
+//  Created by Yago Marques on 12/06/23.
+//
+
+import SwiftUI
+
+@main
+struct BuddyCare_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
